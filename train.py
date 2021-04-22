5bec6cf1cd6d79254ae1920237f0ad623b7f696d
LukasMut <muttenthaler@cbs.mpg.de>
2021-04-22 08:38:47 +0200

    updated code and added cuda devices

--- train.py
+++ train.py
@@ -35,9 +35,6 @@ def parseargs():
     parser = argparse.ArgumentParser()
     def aa(*args, **kwargs):
         parser.add_argument(*args, **kwargs)
-    aa('--version', type=str, default='deterministic',
-        choices=['deterministic', 'variational'],
-        help='whether to apply a deterministic or variational version of SPoSE')
     aa('--task', type=str, default='odd_one_out',
         choices=['odd_one_out', 'similarity_task'])
     aa('--modality', type=str, default='behavioral/',
@@ -102,7 +99,6 @@ def setup_logging(file:str, dir:str='./log_files/'):
     return logger
 
 def run(
-        version:str,
         task:str,
         rnd_seed:int,
         modality:str,
@@ -123,12 +119,9 @@ def run(
         plot_dims:bool=True,
         verbose:bool=True,
 ) -> None:
-    #initialise logger and start logging events
-    logger = setup_logging(file='vspose_model_optimization.log')
-    logger.setLevel(logging.INFO)
     #load triplets into memory
     train_triplets, test_triplets = utils.load_data(device=device, triplets_dir=triplets_dir)
-    n_items = get_nitems(train_triplets)
+    n_items = utils.get_nitems(train_triplets)
     #load train and test mini-batches
     train_batches, val_batches = utils.load_batches(
                                                       train_triplets=train_triplets,
@@ -152,18 +145,22 @@ def run(
     l = torch.ones(batch_size * k, embed_dim).mul(lmbda).to(device)
     n_batches = len(train_batches) #for each mini-batch kld must be scaled by 1/B, where B = n_batches
 
+    #initialise logger and start logging events
+    logger = setup_logging(file='vspose_optimization.log', dir=f'./log_files/{embed_dim}d/seed{rnd_seed:02}/{lmbda}/{weight_decay}')
+    logger.setLevel(logging.INFO)
+
     ################################################
     ############# Creating PATHs ###################
     ################################################
 
     print(f'\n...Creating PATHs.\n')
     if results_dir == './results/':
-        results_dir = pjoin(results_dir, modality, version, f'{embed_dim}d', f'seed{rnd_seed:02d}', str(lmbda), str(weight_decay))
+        results_dir = pjoin(results_dir, modality, 'variational', f'{embed_dim}d', f'seed{rnd_seed:02d}', str(lmbda), str(weight_decay))
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
 
     if plots_dir == './plots/':
-        plots_dir = pjoin(plots_dir, modality, version, f'{embed_dim}d', f'seed{rnd_seed:02d}', str(lmbda), str(weight_decay))
+        plots_dir = pjoin(plots_dir, modality, 'variational', f'{embed_dim}d', f'seed{rnd_seed:02d}', str(lmbda), str(weight_decay))
     if not os.path.exists(plots_dir):
         os.makedirs(plots_dir)
 
@@ -233,13 +230,12 @@ def run(
             batch = batch.to(device)
             logits, mu_hat, l_hat = model(batch, device)
             anchor, positive, negative = torch.unbind(torch.reshape(logits, (-1, 3, embed_dim)), dim=1)
-            #TODO: figure out why the line below is necessary if we don't use the variable probs anywhere else in the script
-            #probs = trinomial_probs(anchor, positive, negative, task, temperature)
             c_entropy = utils.trinomial_loss(anchor, positive, negative, task, temperature)
-            complexity_loss = (1/n_batches) * utils.kld_online(mu_hat, l_hat, mu, l)
-            #NOTE: l2_reg also has to be divided by n_batches, since it is part of the overall prior KLD term
-            l2_reg = (1/n_batches) * utils.l2_reg_(model=model, weight_decay=weight_decay)
-            loss = c_entropy + complexity_loss + l2_reg
+            kld = utils.kld_online(mu_hat, l_hat, mu, l)
+            l2_reg = utils.l2_reg_(model=model, weight_decay=weight_decay)
+            #NOTE: l2_reg also has to be divided by n_batches, since it is part of the complexity term
+            complexity_loss = (1/n_batches) * (kld + l2_reg)
+            loss = c_entropy + complexity_loss
             loss.backward()
             optim.step()
             batch_losses_train[i] += loss.item()
@@ -262,7 +258,7 @@ def run(
         ################ validation ####################
         ################################################
 
-        avg_val_loss, avg_val_acc = utils.validation(model, val_batches, version, task, device, k_samples)
+        avg_val_loss, avg_val_acc = utils.validation(model, val_batches, 'variational', task, device, k_samples)
 
         val_losses.append(avg_val_loss)
         val_accs.append(avg_val_acc)
@@ -278,13 +274,13 @@ def run(
             print(f'====== Epoch: {epoch+1}, Train acc: {avg_train_acc:.3f}, Train loss: {avg_train_loss:.3f}, Val acc: {avg_val_acc:.3f}, Val loss: {avg_val_loss:.3f} ======')
             print("==============================================================================================================\n")
 
-        if (epoch + 1) % 5 == 0:
+        if (epoch + 1) % 20 == 0:
             #sort columns of Ws (i.e., dimensions) in VSPoSE according to their KL divergences in descending order
             sorted_dims, klds_sorted = utils.compute_kld(model, lmbda, aggregate=True, reduction='max')
-            W_mu, W_b = utils.load_weights(model, version)
+            W_mu, W_b = utils.load_weights(model)
             W_l = W_b.pow(-1)
 
-            W_mu_sorted = utils.remove_zeros(W_mu[:, sorted_dims].cpu().T.numpy()).T
+            W_mu_sorted = W_mu[:, sorted_dims].cpu().numpy()
             W_b_sorted = W_b[:, sorted_dims].cpu().numpy()
             W_l_sorted = W_l[:, sorted_dims].cpu().numpy()
 
@@ -294,6 +290,7 @@ def run(
                                 plots_dir=plots_dir,
                                 epoch=int(epoch+1),
                                 )
+
 
             #save model and optim parameters for inference or to resume training
             #PyTorch convention is to save checkpoints as .tar files
@@ -320,7 +317,7 @@ def run(
                     break
 
     #save final model weights (sorted)
-    utils.save_weights_(version, results_dir, W_mu_sorted, W_b_sorted)
+    utils.save_weights_(results_dir, W_mu_sorted, W_b_sorted)
 
     results = {'epoch': len(train_accs), 'train_acc': train_accs[-1], 'val_acc': val_accs[-1], 'val_loss': val_losses[-1]}
     logger.info(f'Optimization finished after {epoch+1} epochs for lambda: {lmbda}\n')
@@ -354,7 +351,6 @@ if __name__ == "__main__":
         device = torch.device(args.device)
 
     run(
-        version=args.version,
         task=args.task,
         rnd_seed=args.rnd_seed,
         modality=args.modality,

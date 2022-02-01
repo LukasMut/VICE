9a6df84f5155ac184e488a51a370d7ca349ce822
LukasMut <muttenthaler@cbs.mpg.de>
2022-02-01 11:30:41 +0100

    added jupyter notebook for VICE

--- visualization.py
+++ visualization.py
@@ -64,7 +64,7 @@ def plot_single_performance(
     ax.plot(val_accs,'-+',  alpha=.5, label='Test')
     ax.plot(train_accs[steps-1::steps], '-+', alpha=.5, label='Train')
     ax.annotate('Val acc: {:.3f}'.format(np.max(val_accs)), (len(val_accs) - len(val_accs) * 0.1, np.max(val_accs) / 2))
-    ax.set_xticks(ticks=range(val_accs), labels=list(range(steps, len(train_accs)+1, steps)))
+    ax.set_xticks(ticks=range(len(val_accs)), labels=list(range(steps, len(train_accs)+1, steps)))
     ax.set_xlabel(r'Epochs')
     ax.set_ylabel(r'Accuracy')
     ax.legend(fancybox=True, shadow=True, loc='best')
@@ -72,6 +72,7 @@ def plot_single_performance(
     PATH = pjoin(plots_dir, 'grid_search')
     if not os.path.exists(PATH):
         os.makedirs(PATH)
+        
     plt.tight_layout()
     plt.savefig(pjoin(PATH, 'single_model_performance_over_time.png'))
     if show_plot:

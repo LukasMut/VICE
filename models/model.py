88804327c1db4e5af497db58d43a3a220debe9d2
LukasMut <muttenthaler@cbs.mpg.de>
2022-02-07 11:40:13 +0100

    refactored model

--- models/model.py
+++ models/model.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import re
 import torch
 
 import numpy as np
@@ -10,11 +9,43 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from models.trainer import Trainer
-from torch.distributions.laplace import Laplace
 from typing import Dict, Tuple
 
 
 os.environ['PYTHONIOENCODING'] = 'UTF-8'
+
+
+class Sigma(nn.Module):
+
+    def __init__(self, in_size: int, out_size: int, bias: bool):
+        super(Sigma, self).__init__()
+        self.in_size = in_size
+        self.out_size = out_size
+        self.logsigma = nn.Linear(
+            self.in_size, self.out_size, bias=bias)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        _ = self.logsigma(x)
+        W_sigma = self.logsigma.weight.T.exp()
+        return W_sigma
+
+
+class Mu(nn.Module):
+
+    def __init__(self, in_size: int, out_size: int, bias: bool):
+        super(Mu, self).__init__()
+        self.in_size = in_size
+        self.out_size = out_size
+        self.mu = nn.Linear(
+            self.in_size, self.out_size, bias=bias)
+        # initialize weights
+        nn.init.kaiming_normal_(
+            self.mu.weight, mode='fan_out', nonlinearity='relu')
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        _ = self.mu(x)
+        W_mu = self.mu.weight.T
+        return W_mu
 
 
 class VICE(Trainer):
@@ -66,9 +97,8 @@ class VICE(Trainer):
         )
         self.in_size = n_items
         self.out_size = latent_dim
-        self.encoder_mu = nn.Linear(self.in_size, self.out_size, bias=bias)
-        self.encoder_logsigma = nn.Linear(
-            self.in_size, self.out_size, bias=bias)
+        self.mu = Mu(self.in_size, self.out_size, bias)
+        self.sigma = Sigma(self.in_size, self.out_size, bias)
 
         if init_weights:
             self._initialize_weights()
@@ -81,30 +111,22 @@ class VICE(Trainer):
 
     def forward(self, x: torch.Tensor
                 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        _ = self.encoder_mu(x)
-        _ = self.encoder_logsigma(x)
-        W_mu = self.encoder_mu.weight.T
-        W_sigma = self.encoder_logsigma.weight.T.exp()
+        W_mu = self.mu(x)
+        W_sigma = self.sigma(x)
         W_sampled = self.reparameterize(W_mu, W_sigma)
         z = torch.mm(x, W_sampled)
         z = F.relu(z)
         return z, W_mu, W_sigma, W_sampled
 
     def _initialize_weights(self) -> None:
-        for n, m in self.named_modules():
-            if isinstance(m, nn.Linear):
-                if re.search(r'mu', n):
-                    nn.init.kaiming_normal_(
-                        m.weight, mode='fan_out', nonlinearity='relu')
-                else:
-                    # this is equivalent to but numerically more stable than mu_std.pow(-1)
-                    eps = (self.encoder_mu.weight.data.std().log() * -1.0).exp()
-                    nn.init.constant_(m.weight, -eps)
+        # this is equivalent to 1/std(mu)
+        eps = -(self.mu.mu.weight.data.std().log() * -1.0).exp()
+        nn.init.constant_(self.sigma.logsigma.weight, eps)
 
     @property
     def detached_params(self) -> Dict[str, np.ndarray]:
-        W_loc = self.encoder_mu.weight.data.T.detach()
-        W_scale = self.encoder_logsigma.weight.data.T.exp().detach()
+        W_loc = self.mu.mu.weight.data.T.detach()
+        W_scale = self.sigma.logsigma.weight.data.T.exp().detach()
         W_loc = F.relu(W_loc)
         params = {'loc': W_loc.cpu().numpy(), 'scale': W_scale.cpu().numpy()}
         return params

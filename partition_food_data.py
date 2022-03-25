138c8bb63b9559efe24742cae77e6b7566df3c1c
LukasMut <muttenthaler@cbs.mpg.de>
2022-03-25 09:16:42 +0100

    added script to partition food data into three sets

--- partition_food_data.py
+++ partition_food_data.py
@@ -7,18 +7,19 @@ import sys
 import numpy as np
 
 from collections import defaultdict
-from typing import Dict, List, Tuple
 
 
-def load_data(in_path: str) -> Tuple[np.ndarray]:
+def load_data(in_path):
     first_half = np.loadtxt(os.path.join(in_path, 'apns1.txt'), dtype=int)
     second_half = np.loadtxt(os.path.join(in_path, 'apns2.txt'), dtype=int)
     return first_half, second_half
 
-def reorder_triplet(t: List[int]) -> Tuple[int]:
+
+def reorder_triplet(t):
     return tuple(sorted(t))
 
-def get_repeats_and_unique_triplets(triplets: np.ndarray) -> Tuple[List[Tuple[int]], Dict[Tuple[int], List[List[int]]]]:
+
+def get_repeats_and_unique_triplets(triplets):
     repeats = defaultdict(list)
     unique_triplets = []
     for t in triplets:
@@ -28,14 +29,12 @@ def get_repeats_and_unique_triplets(triplets: np.ndarray) -> Tuple[List[Tuple[in
         repeats[t_sorted].append(list(t))
     return repeats, unique_triplets
 
-def roll_dice(outcomes: List[int], probabilities: List[float]) -> int:
+
+def roll_dice(outcomes, probabilities):
     return np.random.choice(outcomes, replace=True, p=probabilities)
 
-def partition_triplets(
-    triplets: np.ndarray,
-    repeats: Dict[Tuple[int], List[List[int]]],
-    unique_triplets: List[Tuple[int]],
-    ) -> Tuple[np.ndarray]:
+
+def partition_triplets(triplets, repeats, unique_triplets):
     partition_i = [] # test split
     partition_j = [] # train split
     partition_k = [] # val split
@@ -58,11 +57,13 @@ def partition_triplets(
     partition_k = np.random.permutation(partition_k)
     return partition_i, partition_j, partition_k
 
-def save_partitions(partitions: Tuple[np.ndarray], out_path: str) -> None:
+
+def save_partitions(partitions, out_path):
     file_names = ['test_triplets', 'train_90', 'test_10']
     for i, partition in enumerate(partitions):
         with open(os.path.join(out_path, f'{file_names[i]}.npy'), 'wb') as f:
                   np.save(f, partition)
+
 
 if __name__ == '__main__':
     in_path = sys.argv[1]

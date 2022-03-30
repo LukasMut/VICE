161f9d2e436b6ffab1b6737249e2526be0b67807
Lukas Muttenthaler <LukasMut@users.noreply.github.com>
2022-03-30 10:35:06 +0200

    fixed typo

--- model/vice.py
+++ model/vice.py
@@ -8,7 +8,7 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
-from models.trainer import Trainer
+from model.trainer import Trainer
 from typing import Dict, Tuple
 
 

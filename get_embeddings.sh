8b6b470d15e76bfdc674a19aa88262b7719157d3
LukasMut <muttenthaler@cbs.mpg.de>
2022-05-29 14:37:34 +0200

    added bash script to download embeddings and object names

---
+++ get_embeddings.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+URLS=(
+	"https://raw.githubusercontent.com/LukasMut/VICE/main/data/item_names.tsv"
+	"https://raw.githubusercontent.com/LukasMut/VICE/main/data/things_concepts.tsv"
+	"https://raw.githubusercontent.com/LukasMut/VICE/main/embeddings/things/final_embedding.npy"
+)
+
+FILES=(
+	"item_names.tsv"
+	"things_concepts.tsv"
+	"final_embedding.npy"
+)
+
+data_dir="$(pwd)/data/";
+embedding_dir="$(pwd)/embeddings";
+subdirs=( $data_dir $embedding_dir );
+
+for subdir in ${subdirs[@]}; do
+	if [[ -d $subdir ]]; then
+		printf "\nThe directory $subdir exists\n"
+	else
+		mkdir "$subdir";
+		printf "\nCreated $subdir\n"	
+	fi
+done
+
+
+for i in ${!URLS[@]}; do
+	file=${FILES[i]}
+	url=${URLS[i]}
+	if [[ "$i" == "2" ]]; then
+		subdir=$embedding_dir;
+	else
+		subdir=$data_dir;
+	fi	
+	curl -O "$url";	
+	if [[ -f $file ]]; then
+		echo "$url successfully downloaded"
+		mv "$file" "$subdir"
+	else
+		echo "$url not successfully downloaded"
+		exit -1
+	fi
+done
+cd ..

b5330a4ce312f0eabc64ed35ac6009042d58bccc
LukasMut <muttenthaler@cbs.mpg.de>
2022-11-02 18:06:55 +0100

    added type annotions

--- data/triplet_dataset.py
+++ data/triplet_dataset.py
@@ -2,10 +2,10 @@ from typing import Any
 
 import numpy as np
 import torch
+from torchtyping import TensorType
 
 Tensor = torch.Tensor
 Array = np.ndarray
-
 
 class TripletData(torch.utils.data.Dataset):
     def __init__(self, triplets: Any, n_objects: int):
@@ -17,12 +17,12 @@ class TripletData(torch.utils.data.Dataset):
         else:
             raise TypeError(f'\nData has incorrect type:{type(triplets)}\n')
         self.identity = torch.eye(n_objects).to(self.triplets.device)
-
-    def encode_as_onehot(self, triplet: Tensor) -> Tensor:
-        """Encode a triplet of indices as a matrix of three one-hot-vectors."""
+    
+    def encode_as_onehot(self, triplet: TensorType["k"]) -> TensorType["k", "m"]:
+        """Encode a tensor of three numerical indices as a tensor of three one-hot-vectors."""
         return self.identity[triplet, :]
 
-    def __getitem__(self, index: int) -> Tensor:
+    def __getitem__(self, index: int) -> TensorType["k", "m"]:
         index_triplet = self.triplets[index]
         one_hot_triplet = self.encode_as_onehot(index_triplet)
         return one_hot_triplet
